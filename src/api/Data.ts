9ca29e84fee7cca952d8d5247da8c1e2c6fba134
Michel Nickbon <michel.lemponen@gmail.com>
2024-03-07 12:12:09 +0100

    Cleanup and adjustments

--- src/api/Data.ts
+++ src/api/Data.ts
@@ -1,13 +1,12 @@
-import { uuid } from 'uuidv4';
-import FakeCarOptions from "./CarOptions";
+import CarOptions from "./CarOptions";
 
-export const data: FakeCarOptions[] = [
-    { id: uuid(), category: 'VehicleColor', title: 'blue', description: 'text here', cost: 200 },
-    { id: uuid(), category: 'Engine', title: "T6 Plug-in Hybrid", description: 'text',  cost: 200 },
-    { id: uuid(), category: 'Engine', title: 'D5 Mild Hybrid', description: 'text', cost: 200 },
-    { id: uuid(), category: 'Engine', title: 'D4 Mild Hybrid', description: 'text', cost: 200 },
-    { id: uuid(), category: 'InteriorOptions', title: 'T6 Plug-in Hybrid', description: 'text', cost: 200 },
-    { id: uuid(), category: 'InteriorOptions', title: 'D5 Mild Hybrid', description: 'text', cost: 200 },
-    { id: uuid(), category: 'InteriorOptions', title: 'D4 Mild Hybrid', description: 'text', cost: 200 },
-    { id: uuid(), category: 'ExteriorOptions', title: 'Sport exterior kit', description: 'text', cost: 200 },
+export const data: CarOptions[] = [
+    { id: crypto.randomUUID(), category: 'VehicleColor', title: 'blue', description: 'text here', cost: 200 },
+    { id: crypto.randomUUID(), category: 'Engine', title: "T6 Plug-in Hybrid", description: 'text',  cost: 200 },
+    { id: crypto.randomUUID(), category: 'Engine', title: 'D5 Mild Hybrid', description: 'text', cost: 200 },
+    { id: crypto.randomUUID(), category: 'Engine', title: 'D4 Mild Hybrid', description: 'text', cost: 200 },
+    { id: crypto.randomUUID(), category: 'InteriorOptions', title: 'T6 Plug-in Hybrid', description: 'text', cost: 200 },
+    { id: crypto.randomUUID(), category: 'InteriorOptions', title: 'D5 Mild Hybrid', description: 'text', cost: 200 },
+    { id: crypto.randomUUID(), category: 'InteriorOptions', title: 'D4 Mild Hybrid', description: 'text', cost: 200 },
+    { id: crypto.randomUUID(), category: 'ExteriorOptions', title: 'Sport exterior kit', description: 'text', cost: 200 },
   ];
--- src/components/configurator/ConfiguratorFrame.tsx
+++ src/components/configurator/ConfiguratorFrame.tsx
@@ -1,10 +1,23 @@
 import './ConfiguratorFrame.css';
+import React, { useEffect, useState } from 'react';
+import CarOptions from "../../api/CarOptions";
+import CarOptionsApi from "../../api/CarOptionsApi";
 import ConfiguratorHeader from './ConfiguratorHeader';
 import ConfiguratorSummary from './ConfiguratorSummary';
 import ConfiguratorVehicleView from './ConfiguratorVehicleView';
 import ConfiguratorVehicleOptions from './ConfiguratorVehicleOptions';
 
-function ConfiguratorFrame() {
+const ConfiguratorFrame: React.FC = () => {
+
+  const api = new CarOptionsApi();
+  //const [data, setData] = useState<CarOptions[]>([]);
+
+  // useEffect(() => {
+  //   api.getVehicleColors().then((responseData) => {
+  //     setData(responseData);
+  //   });
+  // }, []);
+
   return (
     <div className="Container">
       <ConfiguratorHeader heading={'Your Volvo V60'} subtitle={'Choose a car model and add equipment, packages and styling to suit your needs.'} />
