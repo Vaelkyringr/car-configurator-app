04e5e2a44398db7f162bc08dee1fbaa809ddec14
Michel Nickbon <michel.lemponen@gmail.com>
2024-03-08 09:05:51 +0100

    Added redux

--- src/components/configurator/ConfiguratorVehicleOptions.tsx
+++ src/components/configurator/ConfiguratorVehicleOptions.tsx
@@ -1,21 +1,28 @@
 import './ConfiguratorVehicleOptions.css';
 import CarOption from "../../api/CarOption";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../state/store";
+import { increment, decrement, incrementByAmount } from "../../state/counter/counterSlice";
 
 interface ConfiguratorVehicleOptionsHeaderProps {
   type: string;
   CarOption: CarOption;
 }
 
-function onOptionSelectedClick() {
-  alert('This will be reported to global state store soon! :-)');
-}
-
 const ConfiguratorVehicleOptions: React.FC<ConfiguratorVehicleOptionsHeaderProps> = ({ type, CarOption }) => {
+
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
+
     return (
-      <div className="configurator-vehicle-option" onClick={onOptionSelectedClick}>
+      <div className="configurator-vehicle-option" /*onClick={onOptionSelectedClick*}*/>
         <h5>{CarOption.title}</h5>
+        <h2>{count}</h2>
         <p>{CarOption.description}</p>
         <p>Price: {CarOption.cost}</p>
+        <button onClick={() => dispatch(increment())}> Increment </button>
+        <button onClick={() => dispatch(incrementByAmount(10))}> Increment by 10 </button>
+        <button onClick={() => dispatch(decrement())}> Decrement </button>
       </div>
     );
   };
